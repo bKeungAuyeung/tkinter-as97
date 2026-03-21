c0b3447e038c0df8febf322ea74a92b312956918
Brandon Auyeung <bauyeung@tuta.com>
2026-03-20 20:59:59 -0700

    Example of AS97 added.

--- as97-calc.py
+++ as97-calc.py
@@ -117,18 +117,101 @@ for i in range(len(as97_const["period"])): # for each period
 Sa_rock_high = np.exp(lnSa_rock_high)
 Sa_rock_low = np.exp(lnSa_rock_low)
 
-fig, ax = subplots(figsize=(4,3), tight_layout=True, dpi=300)
-ax.semilogx(periods, Sa_rock_mean, color = "#980043", label="Mean Ground Motions", linestyle ="-")
-# ax.semilogx(periods, Sa_rock_high, color = "#980043", label=", linestyle = ":")
-# ax.semilogx(periods, Sa_rock_low, color = "#980043", label="PGA Estimate: -1σ", linestyle = ":")
-ax.fill_between(periods, Sa_rock_low, Sa_rock_high, color = "#980043", alpha = 0.2, linestyle = ":", label="$$\pm 1\sigma$$")
+R = np.zeros(len(as97_const["period"])) # initialize R
+for i in range(len(as97_const["period"])): # for each period
+    R[i] = (r_rup**2 + as97_const["c4"][i]**2)**0.5
 
-plt.legend(loc="best", fontsize=7, frameon=False)
+f1_soil = np.zeros(len(as97_const["period"])) # initialize f1
+for i in range(len(as97_const["period"])): # for each period
+    if magnitude <= as97_const["c1"][i]: # if M <= c1...
+        f1_soil[i] = as97_const["a1"][i] + (as97_const["a2"][i] * (magnitude - as97_const["c1"][i])) + (as97_const["a12"][i]*(8.5 - magnitude)**n) + \
+            (as97_const["a3"][i] +(as97_const["a13"][i] * (magnitude-as97_const["c1"][i])))*np.log(R[i]) # collect f1 value
+    else: # if M > c1...
+        f1_soil[i] = as97_const["a1"][i] + (as97_const["a4"][i] * (magnitude - as97_const["c1"][i])) + (as97_const["a12"][i]*(8.5 - magnitude)**n) + \
+            (as97_const["a3"][i] +(as97_const["a13"][i] * (magnitude-as97_const["c1"][i])))*np.log(R[i]) # collect f1 value
+
+# collect f3 values
+f3_soil = np.zeros(len(as97_const["period"])) # initialize f3
+for i in range(len(as97_const["period"])): # for each period
+    if magnitude <= 5.8: # if M <= 5.8...
+        f3_soil[i] = as97_const["a5"][i] # collect f3 value
+    elif 5.8 < magnitude  <= as97_const["c1"][i]: # if M > 5.8 and M <= 6.2...
+        f3_soil[i] = as97_const["a5"][i] + (((as97_const["a6"][i] - as97_const["a5"][i])*(magnitude-5.8))/(as97_const["c1"][i]-5.8)) # collect f3 value
+    else: # if M > 6.2...
+        f3_soil[i] = as97_const["a6"][i] # collect f3 value
+
+# in order to get f4, we need fHW_hw and fHW_r 
+fHW_hw_soil = np.zeros(len(as97_const["period"])) # initialize fHW_hw
+for i in range(len(as97_const["period"])): # for each period
+    if magnitude <= 5.5: # if if M <= 5.5
+        fHW_hw_soil[i] = 0 # collect fHW_hw value
+    elif 5.5 < magnitude  <= 6.5: # if 5.5 < M <= 6.5...
+        fHW_hw_soil[i] = magnitude - 5.5 # collect fHW_hw value
+    else: # if M > 6.5
+        fHW_hw_soil[i] = 1 # collect fHW_hw value
+
+fHW_r_soil = np.zeros(len(as97_const["period"])) # initialize fHW_r
+for i in range(len(as97_const["period"])): # for each period
+    if r_rup < 4: 
+        fHW_r_soil[i] = 0 # collect fHW_r value
+    elif 4 <= r_rup < 8: 
+        fHW_r_soil[i] = (as97_const["a9"][i] * (r_rup - 4))/4 # collect fHW_r value
+    elif 8 <= r_rup < 18: 
+        fHW_r_soil[i] = as97_const["a9"][i] # collect fHW_r value
+    elif 18 <= r_rup < 25: 
+        fHW_r_soil[i] = as97_const["a9"][i] * (((1-(r_rup - 18))/7))  # collect fHW_r value
+    else:
+        fHW_r_soil[i] = 0 # collect soilr value
+
+# finally we can get f4...
+f4_soil = fHW_hw_soil * fHW_r_soil
+
+# collect f5 values
+f5_soil = np.zeros(len(as97_const["period"])) # initialize f5
+
+factors_soil = pd.DataFrame({"f1": f1_soil, "f3": f3_soil, "f4": f4_soil, "f5": f5_soil})
+
+# combine factors with flags to get ln(S_a) and by extension, S_a
+
+lnSa_soil_median = np.zeros(len(as97_const["period"])) # initialize lnSa
+for i in range(len(as97_const["period"])): # for each period
+    if i == 0:
+        loma_pga = np.exp(f1_soil[i] + mechanism * f3_soil[i] + hanging_wall * f4_soil[i])  # collect lnSa value
+        f5_soil[i] = as97_const["a10"][i] + as97_const["a11"][i] * np.log(loma_pga + as97_const["c5"][i]) # collect f5 value
+        lnSa_soil_median[i] = f1_soil[i] + mechanism * f3_soil[i] + hanging_wall * f4_soil[i] + site_type*f5_soil[i]  # collect lnSa value
+    else:
+        for i in range(len(as97_const["period"])): # for each period
+            f5_soil[i] = as97_const["a10"][i] + as97_const["a11"][i] * np.log(loma_pga + as97_const["c5"][i]) # collect f5 value
+            lnSa_soil_median[i] = f1_soil[i] + mechanism * f3_soil[i] + hanging_wall * f4_soil[i] + site_type*f5_soil[i]  # collect lnSa value
+        
+Sa_soil_mean = np.exp(lnSa_soil_median)
+
+# determine +/- 1σ 
+
+lnSa_soil_high = np.zeros(len(as97_const['period']))
+lnSa_soil_low = np.zeros(len(as97_const['period']))
+
+for i in range(len(as97_const["period"])): # for each period
+    lnSa_soil_high[i] = f1_soil[i] + mechanism * f3_soil[i] + hanging_wall * f4_soil[i] + site_type*f5_soil[i] + sigma[i] # collect lnSa value
+    lnSa_soil_low[i] = f1_soil[i] + mechanism * f3_soil[i] + hanging_wall * f4_soil[i] + site_type*f5_soil[i] - sigma[i] # collect lnSa value
+
+Sa_soil_high = np.exp(lnSa_soil_high)
+Sa_soil_low = np.exp(lnSa_soil_low)
+
+factors_soil["f5"] = f5_soil
+
+fig, ax = subplots(figsize=(4,3), tight_layout=True, dpi=300)
+if site_type == 1: # if site type is soil
+    ax.semilogx(periods, Sa_soil_mean, color = "#980043", label="Soil Spectra", linestyle = "-")
+    ax.fill_between(periods, Sa_soil_low, Sa_soil_high, color = "#980043", alpha = 0.2, linestyle = ":")
+else: # if site type is rock
+    ax.semilogx(periods, Sa_rock_mean, color = "#810f7c", label="Rock Spectra", linestyle ="-")
+    ax.fill_between(periods, Sa_rock_low, Sa_rock_high, color = "#810f7c", alpha = 0.2, linestyle = "--")
 
 ax.set_xlabel("Structural Period [sec]", fontsize = 8)
 ax.set_xbound(0.01, 5.2)
-ax.set_ylabel("$S_a$ on Rock Site [G]", fontsize =8)
+ax.set_ylabel("$S_a$ [g]", fontsize =8)
 ax.set_ybound(0, 2)
 ax.tick_params(which="both", direction = "in", labelsize = 8)
-
+plt.legend(frameon = False, fontsize = 8, loc = "best")
 plt.show()
---
+++ gui.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Anderson & Silva (1997)")
+root.geometry("400x300")
+
+root.mainloop()
